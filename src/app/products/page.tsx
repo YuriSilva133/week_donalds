2c60a865a6beb8db5503cfc83adf843541911498
Yuri Silva <yuriteofilo38@gmail.com>
2025-02-22 19:01:19 -0300

    feat: add initial restaurant page and menu page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,10 +3,10 @@ import { Input } from "@/components/ui/input";
 
 const ProductPage = () => {
   return (
-    <div className="p-5 border border-red-500 rounded-xl">
+    <div className="rounded-xl border border-red-500 p-5">
       <h1 className="text-red-500">Product Page</h1>
-        <Button> Click me </Button>
-        <Input placeholder="Enter your email" />
+      <Button> Click me </Button>
+      <Input placeholder="Enter your email" />
     </div>
   );
 };
